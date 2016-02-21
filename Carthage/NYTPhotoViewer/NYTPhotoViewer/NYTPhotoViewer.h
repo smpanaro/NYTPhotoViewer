433002a940a0ed08be9b829aed579ed19804636e
Stephen Panaro <stephen.panaro@gmail.com>
2016-02-21 17:59:07 -0500

    Add separate project with shared scheme for Carthage

---
+++ Carthage/NYTPhotoViewer/NYTPhotoViewer/NYTPhotoViewer.h
@@ -0,0 +1,21 @@
+//
+//  NYTPhotoViewer.h
+//  NYTPhotoViewer
+//
+//  Created by Stephen Panaro on 2/21/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NYTPhotoViewer.
+FOUNDATION_EXPORT double NYTPhotoViewerVersionNumber;
+
+//! Project version string for NYTPhotoViewer.
+FOUNDATION_EXPORT const unsigned char NYTPhotoViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NYTPhotoViewer/PublicHeader.h>
+
+#import <NYTPhotoViewer/NYTPhotosViewController.h>
+#import <NYTPhotoViewer/NYTPhoto.h>
+#import <NYTPhotoViewer/NYTPhotoContainer.h>
